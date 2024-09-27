a1f02c10446a3db9283880a25b8fdf40b9124472
Dylan Hartman <156948060+DylanHartmann@users.noreply.github.com>
2024-09-27 14:40:06 -0400

    Add files via upload

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,6 +1,44 @@
-document.write("Running fine!")
+const path = "127.0.0.1:5500";
+const information_file = "ShorseInformation";
+var file_name = "WhereShorse";
 
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
+var files = ["Home", "WhereShorse", "WhatShorse", "test"];
 
-const file_service = require("node:fs");
+// get file information
+function setInformation(location) {
+    var goto_path = "http://"+path+"/"+information_file+"/"+location+".md"
+    var element = document.getElementById("zero-md-element");
+    element.src = information_file+"/"+location+".md"
+    /*
+    fetch(goto_path)
+        .then(response => response.text())
+        .then(data => {
+            var element = document.getElementById("zero-md-element");
+            element.src = location=".md"
+            document.getElementById('output').textContent = data;
+        })
+        .catch(error => console.error('Error fetching text file: ', error));*/
+};
+
+// Setup all buttons onto page
+for (var i = 0; i<files.length; i++) {
+    // Get that specific file name
+    var temp = files[i];
+
+    // Create the button
+    const button = document.createElement('button');
+    button.innerText = temp;
+
+    // Add button event listeners
+    button.addEventListener('click', () => {
+        var name = button.innerText;
+        console.log(name);
+        setInformation(name);
+    })
+
+    // do a bunch of adding
+    var li = document.createElement("li");
+    li.appendChild(button)
+    li.setAttribute("id", "md_button");
+    document.getElementById("Out-Nav").appendChild(li);
+}
